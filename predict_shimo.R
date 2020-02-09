529b34704d5d255e34a0e251e86caa830186938a
進撃の雯雯老師 <askahu6@gmail.com>
2020-02-09 12:20:14 +0800

    Update predict_shimo.R

--- predict_shimo.R
+++ predict_shimo.R
@@ -4,81 +4,121 @@ library(openxlsx)
 library(data.table)
 library(dplyr)
 library(keras)
+check <- function(x){data.frame(date=datemap,raw[names(raw)==x])}
+
+############################
+# Data Processing
+############################
 
 setwd('/Users/wenrurumon/Documents/posdoc/wuhan')
-raw <- read.xlsx('data0208.xlsx')
-
+raw <- read.xlsx('data0209.xlsx')
 colnames(raw) <- c('date','province','city','confirm','heal','dead','source')
-othnat <- "新加坡,台湾,澳门,马来西亚,日本,韩国,美国,香港,澳大利亚,菲律宾,加拿大,泰国,比利时,越南,德国,印度,阿联酋,斯里兰卡,西班牙,瑞典,俄罗斯,英国,意大利,法国,芬兰,柬埔寨,尼泊尔"
-othnat <- strsplit(othnat,',')[[1]]
-raw <- mutate(raw,city=ifelse(is.na(city),province,city)) %>% filter(!(province%in%othnat))
-raw$date <- sapply(strsplit(raw$date,'月|日'),function(x){as.numeric(x[1])*100+as.numeric(x[2])})
-raw <- raw %>% arrange(date,province,city) %>% 
+nat <- "湖北,上海,北京,四川,广东,云南,天津,山东,河南,浙江,重庆,黑龙江,宁夏,安徽,山西,广西,江苏,江西,河北,海南,湖南,福建,贵州,辽宁,内蒙古,吉林,新疆,甘肃,陕西,青海,西藏"
+nat <- strsplit(nat,',')[[1]]
+
+raw <- mutate(raw,city=ifelse(is.na(city),province,city)) %>% 
+  arrange(date,province,city) %>% 
   group_by(date,province,city) %>% 
-  summarise(confirm=sum(confirm),heal=sum(heal),dead=sum(dead)) %>% 
-  mutate(citykey=paste(date,city),provkey=paste(date,province)) 
-raw.date <- c(min(raw$date):131,201:max(raw$date))
+  summarise(confirm=sum(confirm),heal=sum(heal),dead=sum(dead))
+raw$date <- sapply(strsplit(raw$date,'月|日'),function(x){as.numeric(x[1])*100+as.numeric(x[2])})
+raw <- raw %>% group_by(date,province,city) %>% summarise(
+  confirm = max(confirm), heal=max(heal),dead=max(dead)
+)
 
-raw.city <- lapply(unique(raw$province),function(i){
-  x <- (raw[match(paste(raw.date,i),raw$provkey),])
+raw <- raw %>% mutate(citykey=paste(date,city),provkey=paste(date,province)) 
+datemap <- c(min(raw$date):131,201:max(raw$date))
+citymap <- unique(select(as.data.frame(raw),province,city))
+raw <- raw %>% group_by(provkey,date,province) %>% 
+  summarise(confirm=sum(confirm),heal=sum(heal),dead=sum(dead))
+raw.idx <- raw %>% group_by(province) %>% summarise(
+  cidx = sum(confirm), hidx = sum(heal), didx = sum(dead)
+)
+raw <- lapply(unique(citymap$province),function(i){
+  x <- (raw[match(paste(datemap,i),raw$provkey),])
   x[is.na(x)] <- 0
-  apply(select(as.data.frame(x),confirm,heal,dead),2,cumsum)
+  x <- apply(select(as.data.frame(x),confirm,heal,dead),2,cumsum)
+  x
 })
-names(raw.city) <- unique(raw$province)
+names(raw) <- unique(citymap$province)
+raw.cn <- sapply(raw[(names(raw)%in%nat)],function(x){x[,1]})
 
-data.model <- sapply(raw.city,function(x){x[,1]})
-plot.ts(rowSums(data.model))
-data.model.max <- max(data.model)*1.2
-data.model <- data.model / data.model.max
+# raw.cn <- log(raw.cn+1)
+f.cn <- max(raw.cn)*1.2
+raw.cn <- raw.cn/f.cn
 
-data.model <- lapply(1:(nrow(data.model)-12),function(i){
-  list(x=t(data.model[0:9+i,]),y=t(data.model[10:12+i,]))
-})
-X <- do.call(rbind,lapply(data.model[1:(length(data.model)-2)],function(x){x$x}))
-Y <- do.call(rbind,lapply(data.model[1:(length(data.model)-2)],function(x){x$y}))
+############################
+# Model File
+############################
 
-############
+# x <- raw.cn
+# i <- 1
+# p <- 8
+# h <- 1
+get_model_file <- function(x,i,p,h){
+  y <- t(x[1:h-1+p+i,,drop=F])
+  x <- t(x[(1:p)-1+i,,drop=F])
+  y <- y/x[,ncol(x)]
+  y[is.na(y)] <- 1
+  y[is.infinite(y)] <- 1
+  y <- log(y)
+  # y <- y-1
+  list(x=x,y=y)
+}
+process_model_file <- function(data.model){
+  X <- do.call(rbind,lapply(data.model,function(x){x$x}))
+  Y <- do.call(rbind,lapply(data.model,function(x){x$y}))
+  list(X=X,Y=Y)
+}
 
+############################
+# Modeling China
+############################
+
+#China by city
+
+p <- 8
+h <- 1
+train.cn <- lapply(1:(length(datemap)-p),get_model_file,h=h,p=p,x=raw.cn) 
+# w <- c(1:18,0,0,0)
+w <- c(rep(1,18),0,0,0)
+train.cn <- rep(train.cn,w) %>% process_model_file()
+
+X <- train.cn$X
+Y <- train.cn$Y
 dims <- c(32,16,4)
+models <- rep('sigmoid',4)
 e.input <- layer_input(shape=ncol(X))
-e.layer <- layer_dense(e.input,dims[1],activation='sigmoid')
-l.layer <- layer_dense(e.layer,dims[2],activation='sigmoid')
-d.layer <- layer_dense(units=dims[3],activation='sigmoid')
-d.output <- layer_dense(units=ncol(Y),activation='sigmoid')
+e.layer <- layer_dense(e.input,dims[1],activation=models[1])
+l.layer <- layer_dense(e.layer,dims[2],activation=models[2])
+d.layer <- layer_dense(units=dims[3],activation=models[3])
+d.output <- layer_dense(units=ncol(Y),activation=models[4])
 d.input <- layer_input(shape=dims[3])
 model <- keras_model(e.input,d.output(d.layer(l.layer)))
 model %>% compile(
   loss = "mean_squared_error", 
   optimizer = "adam",
-  metrics = c('accuracy')
+  metrics = c('mae')
 )
-history <- model %>% fit(
+system.time(history <- model %>% fit(
   x = X, 
   y = Y,
   epochs = 4000,
   verbose = 2
-)
+))
 
-############
+get_x <- function(x,i,p,h){
+  x <- t(x[(1:p)-1+i,,drop=F])
+  x
+}
 
-Y.fit <- model %>% predict(do.call(rbind,lapply(data.model,function(x){x$x})))
-Y.actual <- do.call(rbind,lapply(data.model,function(x){x$y}))
-Y.fit <- data.table(
-  city = rownames(Y.actual),
-  batch = rep(1:(nrow(Y.actual)/31),31),
-  Y.fit
-) %>% group_by(batch) %>% 
-  summarise(day1=sum(V1),day2=sum(V2),day3=sum(V3))
-Y.actual <- data.table(
-  city = rownames(Y.actual),
-  batch = rep(1:(nrow(Y.actual)/31),31),
-  Y.actual
-) %>% group_by(batch) %>% 
-  summarise(day1=sum(V1),day2=sum(V2),day3=sum(V3))
+Y.fit <- sapply((9:29)-8,function(i){
+    temp <- get_x(raw.cn,i,p,h)
+    temp <- temp[,ncol(temp)] * exp((model %>% predict(temp)))
+    # temp <- temp[,ncol(temp)] * ((model %>% predict(temp))+1)
+    # temp <- model %>% predict(temp)
+    sum(temp)
+  }) * f.cn
+Y.actual <- rowSums(raw.cn)[-1:-8] * f.cn
+cbind(actual = Y.actual, fit = Y.fit, error = Y.fit/Y.actual-1)
 
-Y.validate <- data.table(
-  class = c(rep('train',(length(data.model)-2)),c(rep('test',2))),
-  Y.fit[,-1]/Y.actual[,-1]-1
-) %>% print
-Y.validate %>% group_by(class) %>% 
-  summarise(day1=mean(abs(day1)),day2=mean(abs(day2)),day3=mean(abs(day3)))
+
