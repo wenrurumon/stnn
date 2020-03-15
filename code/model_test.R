f6bb9dd5a8d3ad186d35bb3b56d2e43914c0c7b3
進撃の雯雯老師 <askahu6@gmail.com>
2020-03-15 21:32:53 +0800

    Update model_test.R

--- code/model_test.R
+++ code/model_test.R
@@ -37,6 +37,25 @@ MSAE <- function(X,Y,dims,activations,batch,epochs,verbose){
   ))
   list(model=model,encoder=encoder,decoder=decoder,history=history)
 }
+getx <- function(temp,keyi){
+  x <- filter(temp,date>max(temp$date)-8*3600*24&key==keyi)
+  d <- mean(x$dum)
+  f <- mean(x$case+1)
+  list(log=x[nrow(x),],x=c(x=log(x$case/f+1),d=d,f=f,w=x$w[nrow(x)]))
+}
+refreshi <- function(temp,d=0){
+  do.call(rbind,lapply(unique(temp$key),function(keyi){
+    x <- getx(temp,keyi)
+    y <- sapply(model.test,function(m){m$model %>% predict(matrix(x$x,nrow=1))})
+    y <- list(y=round(mean((exp(y)-1)*x$x[10])))
+    y$log <- x$log
+    y$log$date <- y$log$date+3600*24
+    y$log$case <- y$y
+    y$log$acase <- y$log$acase+y$log$case
+    y$log$dum <- d
+    y$log
+  }))
+}
 
 ############################
 #Modeling
@@ -50,7 +69,7 @@ mfile.y <- cbind(y=sapply(mfile,function(x){c(x$y)}))
 
 #Modeling
 
-models.test<- lapply(
+model.test<- lapply(
   paste0('/Users/wenrurumon/Documents/posdoc/wuhan/model/test_',1:5,'.model'),
   function(x){list(model=keras::load_model_hdf5(x))})
 # model.test <- lapply(1:5,function(i){
@@ -64,25 +83,33 @@ models.test<- lapply(
 
 #Validation Data
 
-rm(mfile)
-load('/Users/wenrurumon/Documents/posdoc/wuhan/data/mfile.rda')
-mfile.x <- sapply(mfile,function(x){c(x$x,f=x$f,w=x$w)}) %>% t
-mfile.y <- cbind(y=sapply(mfile,function(x){c(x$y)}))
-mfile.key <- do.call(rbind,lapply(mfile,function(x){
-  select(x$log$y,date,scope,state,acase)
-}))
-rlt.test <- lapply(model.test,function(m){
-  x <- data.frame(mfile.key,f=mfile.x[,10],y=mfile.y,
-             pred=m$model %>% predict(mfile.x)) %>% mutate(
-               y = (exp(y)-1)*f, pred = round((exp(pred)-1)*f)
-             ) %>% 
-    group_by(date,scope) %>% 
-    summarise(acase=sum(acase),y=sum(y),pred=sum(pred)) %>% 
-    mutate(apred=acase-y+pred,error=(apred-acase)^2)
-  x <- merge(x,x %>% group_by(scope) %>% summarise(my=mean(acase)),by='scope') %>% 
-    mutate(bench=(my-acase)^2)
-  x %>% group_by(scope) %>% summarise(error=mean(error)/mean(bench))
-})
+# rm(mfile)
+# load('/Users/wenrurumon/Documents/posdoc/wuhan/data/mfile.rda')
+# mfile.x <- sapply(mfile,function(x){c(x$x,f=x$f,w=x$w)}) %>% t
+# mfile.y <- cbind(y=sapply(mfile,function(x){c(x$y)}))
+# mfile.key <- do.call(rbind,lapply(mfile,function(x){
+#   select(x$log$y,date,scope,state,acase)
+# }))
+# rlt.test <- lapply(model.test,function(m){
+#   x <- data.frame(mfile.key,f=mfile.x[,10],y=mfile.y,
+#                   pred=m$model %>% predict(mfile.x)) %>% mutate(
+#                     y = (exp(y)-1)*f, pred = round((exp(pred)-1)*f)
+#                   ) %>% 
+#     group_by(date,scope) %>% 
+#     summarise(acase=sum(acase),y=sum(y),pred=sum(pred)) %>% 
+#     mutate(apred=acase-y+pred,error=(apred-acase)^2)
+#   x <- merge(x,x %>% group_by(scope) %>% summarise(my=mean(acase)),by='scope') %>% 
+#     mutate(bench=(my-acase)^2)
+#   x %>% group_by(scope) %>% summarise(error=mean(error)/mean(bench))
+# })
 
 #Prediction
+
+temp <- filter(raw,scope=='USA')
+temp <- filter(temp,date>max(temp$date)-8*3600*24)
+for(i in 1:300){
+  print(i)
+  tempi <- refreshi(temp)
+  temp <- rbind(temp,tempi)
+}
 
