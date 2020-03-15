081dd5828b163d885a862afa20d556a17c3e6582
進撃の雯雯老師 <askahu6@gmail.com>
2020-03-15 19:50:49 +0800

    Update model1.R

--- code/model1.R
+++ code/model1.R
@@ -9,13 +9,6 @@ library(keras)
 ############################
 
 load('/Users/wenrurumon/Documents/posdoc/wuhan/data/mfile.rda')
-mfile.w <- ceiling((sapply(mfile,function(x){x$w})+1)/14)
-mfile <- rep(mfile,mfile.w)
-mfile.x <- sapply(mfile,function(x){c(x$x,f=x$f,w=x$w)}) %>% t
-mfile.y <- sapply(mfile,function(x){c(x$y)})
-mfile.key <- do.call(rbind,lapply(mfile,function(x){select(x$log$y,date,scope,state)}))
-mfile <- data.table(mfile.key,mfile.x,y=mfile.y)
-head(mfile)
 
 ############################
 #Module
@@ -45,16 +38,43 @@ MSAE <- function(X,Y,dims,activations,batch,epochs,verbose){
   list(model=model,encoder=encoder,decoder=decoder,history=history)
 }
 
-X <- mfile %>% select(x1,x2,x3,x4,x5,x6,x7,x8,d,f,w) %>% as.matrix
-Y <- mfile %>% select(y) %>% as.matrix
-system.time(model.test <- MSAE(
-  X=X,Y=Y,
-  dims=c(8,4),activations=c('relu','relu'),
-  batch=128,epochs=1000,verbose=1))
+############################
+#Modeling
+############################
 
-mfile2 <- data.frame(mfile,pred=model.test$model %>% predict(X)) %>% mutate(
-  y = (exp(y)-1)*f, pred = (exp(pred)-1)*f
-)
-mfile2 <- mfile2 %>% group_by(date,scope,state) %>% summarise(y=sum(y),pred=sum(pred))
+#Model Data
+mfile.w <- ceiling((sapply(mfile,function(x){x$w})+1)/14)
+mfile <- rep(mfile,mfile.w)
+mfile.x <- sapply(mfile,function(x){c(x$x,f=x$f,w=x$w)}) %>% t
+mfile.y <- cbind(y=sapply(mfile,function(x){c(x$y)}))
 
+#Modeling
+model.test <- lapply(1:5,function(i){
+  print(paste(i,Sys.time()))
+  MSAE(
+    X=mfile.x,Y=mfile.y,
+    dims=c(16,4),activations=c('relu','relu'),
+    batch=128,epochs=1000,verbose=0)
+})
 
+#Validation Data
+
+rm(mfile)
+load('/Users/wenrurumon/Documents/posdoc/wuhan/data/mfile.rda')
+mfile.x <- sapply(mfile,function(x){c(x$x,f=x$f,w=x$w)}) %>% t
+mfile.y <- cbind(y=sapply(mfile,function(x){c(x$y)}))
+mfile.key <- do.call(rbind,lapply(mfile,function(x){
+  select(x$log$y,date,scope,state,acase)
+}))
+rlt.test <- lapply(model.test,function(m){
+  x <- data.frame(mfile.key,f=mfile.x[,10],y=mfile.y,
+             pred=m$model %>% predict(mfile.x)) %>% mutate(
+               y = (exp(y)-1)*f, pred = round((exp(pred)-1)*f)
+             ) %>% 
+    group_by(date,scope) %>% 
+    summarise(acase=sum(acase),y=sum(y),pred=sum(pred)) %>% 
+    mutate(apred=acase-y+pred,error=(apred-acase)^2)
+  x <- merge(x,x %>% group_by(scope) %>% summarise(my=mean(acase)),by='scope') %>% 
+    mutate(bench=(my-acase)^2)
+  x %>% group_by(scope) %>% summarise(error=mean(error)/mean(bench))
+})
