91df2ed52ba8a97dcd39c75ac8a9a017a9f47327
進撃の雯雯老師 <askahu6@gmail.com>
2020-03-16 20:06:29 +0800

    Update processdata.R

--- code/processdata.R
+++ code/processdata.R
@@ -7,8 +7,8 @@ library(dplyr)
 rmna <- function(x){ifelse(is.na(x),0,x)}
 
 setwd('/Users/wenrurumon/Documents/posdoc/wuhan/data')
-raw1 <- read.csv('china_confirmed_314.csv')[,-1]
-raw2 <- read.csv("Cov19_314.csv") %>% 
+raw1 <- read.csv('china_confirmed_315.csv')[,-1]
+raw2 <- read.csv("Cov19_315.csv") %>% 
   mutate(state=tolower(state),dead=rmna(death),confirmed=rmna(confirmed)) %>% 
   select(date,state,dead,confirmed)
 raw2[raw2$state=='china'&raw2$date<=213,]$confirmed <- rowSums(raw1[11:34,-1])
@@ -22,7 +22,7 @@ raw <- lapply(unique(raw$state),function(s){
 names(raw) <- sapply(raw,function(x){x$state})
 raw.global <- sapply(raw,function(x){x$confirmed})
 raw.china <- raw1[,-1]
-raw.usa <- read.csv('usa.csv')[,-1:-2]
+raw.usa <- read.csv('usa_0315.csv')[,-1:-2]
 raw.usa <- apply(raw.usa,2,cumsum)
 
 raw.date <- as.POSIXct( "2020-01-20") + 3600*24*1:length(1:nrow(raw.global))
